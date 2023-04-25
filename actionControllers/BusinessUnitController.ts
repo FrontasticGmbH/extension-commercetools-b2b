e81c0062921404e52e66b02829e8f6140dbb6abf
Carlos Mourullo <carlosmourullo@gmail.com>
2023-04-25 17:07:43 +0200

    refactor: removed class b2b businessunit controller

--- actionControllers/BusinessUnitController.ts
+++ actionControllers/BusinessUnitController.ts
@@ -1,12 +1,14 @@
-export * from './B2BBusinessUnitController';
+import { AccountApi } from "@Commerce-commercetools/apis/AccountApi";
+
 import { ActionContext, Request, Response } from '@frontastic/extension-types';
 import { AccountRegisterBody } from './AccountController';
-import { Store } from '@Types/store/Store';
+import { Store, StoreKeyReference } from "@Types/store/Store";
 import { getLocale } from '../utils/Request';
 import { StoreApi } from '../apis/StoreApi';
 import { BusinessUnitApi } from '../apis/BusinessUnitApi';
 import { CartApi } from '../apis/CartApi';
 import { BusinessUnitMapper } from '../mappers/BusinessUnitMapper';
+import { BusinessUnit, BusinessUnitStatus, BusinessUnitType, StoreMode } from "@Types/business-unit/BusinessUnit";
 
 type ActionHook = (request: Request, actionContext: ActionContext) => Promise<Response>;
 
@@ -18,6 +20,21 @@ export interface BusinessUnitRequestBody {
     accountId: string;
   };
 }
+
+export const getMe: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const organization = request.sessionData?.organization;
+  let businessUnit = organization?.businessUnit;
+
+  if (request.sessionData?.account?.accountId && !businessUnit) {
+    const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+    businessUnit = await businessUnitApi.getMe(request.sessionData?.account?.accountId);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(businessUnit),
+  };
+};
 
 export const setMe: ActionHook = async (request: Request, actionContext: ActionContext) => {
   const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
@@ -46,6 +63,49 @@ export const setMe: ActionHook = async (request: Request, actionContext: ActionC
   return response;
 };
 
+export const getMyOrganization: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+
+  const allOrganization = await businessUnitApi.getTree(request.sessionData?.account?.accountId);
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(allOrganization),
+    sessionData: request.sessionData,
+  };
+
+  return response;
+};
+
+export const getSuperUserBusinessUnits: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const config = actionContext.frontasticContext?.project?.configuration?.associateRoles;
+  if (!config?.defaultSuperUserRoleKey) {
+    throw new Error('Configuration error. No "defaultSuperUserRoleKey" exists');
+  }
+  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request));
+  const customerAccount = await accountApi.getCustomerByEmail(request.query.email);
+  if (customerAccount) {
+    const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+    const results = await businessUnitApi.getAssociatedBusinessUnits(customerAccount.id);
+    const highestNodes = businessUnitApi.getHighestNodesWithAssociation(results, customerAccount.id);
+
+    const businessUnitsWithSuperUser = highestNodes.filter((bu) =>
+      BusinessUnitMapper.isUserAdminInBusinessUnit(bu, customerAccount.id, config.defaultSuperUserRoleKey),
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(businessUnitsWithSuperUser),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      errorCode: 400,
+      error: 'Customer not found',
+    };
+  }
+};
+
 export const getBusinessUnitOrders: ActionHook = async (request: Request, actionContext: ActionContext) => {
   const cartApi = new CartApi(
     actionContext.frontasticContext,
@@ -69,3 +129,282 @@ export const getBusinessUnitOrders: ActionHook = async (request: Request, action
 
   return response;
 };
+
+export const create: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+  const config = actionContext.frontasticContext?.project?.configuration?.associateRoles;
+  if (!config?.defaultBuyerRoleKey || !config?.defaultAdminRoleKey) {
+    return {
+      statusCode: 400,
+      error: 'No associateRoles context defined',
+      errorCode: 400,
+    };
+  }
+  const data = mapRequestToBusinessUnit(request, config);
+
+  const store = await businessUnitApi.create(data);
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(store),
+    sessionData: request.sessionData,
+  };
+
+  return response;
+};
+
+export const addAssociate: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request));
+  const addUserBody: { email: string; roles: string[] } = JSON.parse(request.body);
+
+  const account = await accountApi.getCustomerByEmail(addUserBody.email);
+  if (!account) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'User not found' }),
+      sessionData: request.sessionData,
+    };
+  }
+
+  const businessUnit = await businessUnitApi.update(request.query['key'], [
+    {
+      action: 'addAssociate',
+      associate: {
+        customer: {
+          typeId: 'customer',
+          id: account.id,
+        },
+        associateRoleAssignments: addUserBody.roles.map((role) => ({
+          associateRole: {
+            typeId: 'associate-role',
+            key: role,
+          },
+        })),
+      },
+    },
+  ]);
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(businessUnit),
+    sessionData: request.sessionData,
+  };
+
+  return response;
+};
+
+export const removeAssociate: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+
+  const { id } = JSON.parse(request.body);
+
+  const businessUnit = await businessUnitApi.update(request.query['key'], [
+    {
+      action: 'removeAssociate',
+      customer: {
+        typeId: 'customer',
+        id,
+      },
+    },
+  ]);
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(businessUnit),
+    sessionData: request.sessionData,
+  };
+
+  return response;
+};
+
+export const updateAssociate: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+
+  const { id, roles }: { id: string; roles: string[] } = JSON.parse(request.body);
+
+  const businessUnit = await businessUnitApi.update(request.query['key'], [
+    {
+      action: 'changeAssociate',
+      associate: {
+        customer: {
+          typeId: 'customer',
+          id,
+        },
+        associateRoleAssignments: roles.map((role) => ({
+          associateRole: {
+            typeId: 'associate-role',
+            key: role,
+          },
+        })),
+      },
+    },
+  ]);
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(businessUnit),
+    sessionData: request.sessionData,
+  };
+
+  return response;
+};
+
+export const update: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+  const { key, actions } = JSON.parse(request.body);
+
+  const businessUnit = await businessUnitApi.update(key, actions);
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(businessUnit),
+    sessionData: {
+      ...request.sessionData,
+      organization: {
+        // TODO
+        ...request.sessionData?.organization,
+        businessUnit,
+      },
+    },
+  };
+
+  return response;
+};
+
+export const getByKey: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+  try {
+    const businessUnit = await businessUnitApi.getByKey(request.query?.['key']);
+
+    const response: Response = {
+      statusCode: 200,
+      body: JSON.stringify(businessUnit),
+      sessionData: request.sessionData,
+    };
+
+    return response;
+  } catch {
+    const response: Response = {
+      statusCode: 400,
+      // @ts-ignore
+      error: new Error('Business unit not found'),
+      errorCode: 400,
+    };
+
+    return response;
+  }
+};
+
+export const remove: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+  const key = request.query?.['key'];
+
+  let response: Response;
+
+  try {
+    const businessUnit = await businessUnitApi.delete(key);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(businessUnit),
+      sessionData: request.sessionData,
+    };
+  } catch (e) {
+    response = {
+      statusCode: 400,
+      sessionData: request.sessionData,
+      // @ts-ignore
+      error: e?.body?.message,
+      errorCode: 500,
+    };
+  }
+
+  return response;
+};
+
+export const query: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+
+  let where = '';
+  if ('where' in request.query) {
+    where += [request.query['where']];
+  }
+  const store = await businessUnitApi.query(where);
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(store),
+    sessionData: request.sessionData,
+  };
+
+  return response;
+};
+
+function mapRequestToBusinessUnit(request: Request, config: Record<string, string>): BusinessUnit {
+  const businessUnitBody: BusinessUnitRequestBody = JSON.parse(request.body);
+  const normalizedName = businessUnitBody.account.company.toLowerCase().replace(/ /g, '_');
+  const key = businessUnitBody.parentBusinessUnit
+    ? `${businessUnitBody.parentBusinessUnit}_div_${normalizedName}`
+    : `business_unit_${normalizedName}`;
+
+  let storeMode = StoreMode.Explicit;
+  let unitType = BusinessUnitType.Company;
+  const stores: StoreKeyReference[] = [];
+
+  if (businessUnitBody.parentBusinessUnit && !businessUnitBody.store) {
+    storeMode = StoreMode.FromParent;
+  }
+
+  if (businessUnitBody.parentBusinessUnit) {
+    unitType = BusinessUnitType.Division;
+  }
+
+  if (businessUnitBody.store) {
+    stores.push({
+      typeId: 'store',
+      id: businessUnitBody.store.id,
+    });
+  }
+
+  const businessUnit: BusinessUnit = {
+    key,
+    name: businessUnitBody.account.company,
+    status: BusinessUnitStatus.Active,
+    stores,
+    storeMode,
+    unitType,
+    contactEmail: businessUnitBody.account.email,
+    associates: [
+      {
+        associateRoleAssignments: [
+          {
+            associateRole: {
+              key: config.defaultBuyerRoleKey,
+              typeId: 'associate-role',
+            },
+          },
+          {
+            associateRole: {
+              key: config.defaultAdminRoleKey,
+              typeId: 'associate-role',
+            },
+          },
+        ],
+        customer: {
+          id: businessUnitBody.customer.accountId,
+          typeId: 'customer',
+        },
+      },
+    ],
+  };
+
+  if (businessUnitBody.parentBusinessUnit) {
+    businessUnit.parentUnit = {
+      key: businessUnitBody.parentBusinessUnit,
+      typeId: 'business-unit',
+    };
+  }
+
+  return businessUnit;
+}
+
