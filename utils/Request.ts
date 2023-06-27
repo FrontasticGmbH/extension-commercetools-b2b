5b0b34867e95c1ba08cb06a230e0cd99f7bc4609
Carlos Mourullo <carlosmourullo@gmail.com>
2023-06-27 13:22:15 +0200

    feat: set currency as EUR in those places where is hardcoded for en_GB

--- utils/Request.ts
+++ utils/Request.ts
@@ -15,7 +15,7 @@ export const getLocale = (request: Request): string => {
 };
 
 export const getCountry = (locale: string) => {
-  return { de_DE: 'DE', en_GB: 'GB', 'de_DE@EUR': 'DE', 'en_GB@GBP': 'GB' }[locale];
+  return { de_DE: 'DE', en_GB: 'GB', 'de_DE@EUR': 'DE', 'en_GB@EUR': 'GB' }[locale];
 };
 
 export const getCurrency = (request: Request): string | null => {
