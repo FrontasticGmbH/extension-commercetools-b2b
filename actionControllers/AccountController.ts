bd11b5c724268fe10a13a71c4a37638c4bc40609
JoelFickson <Joelngozo@gmail.com>
2023-07-03 12:34:46 +0200

    feat: update AccountController

--- actionControllers/AccountController.ts
+++ actionControllers/AccountController.ts
@@ -1,16 +1,8 @@
-import { AccountAuthenticationError } from '@Commerce-commercetools/errors/AccountAuthenticationError';
-import { ActionContext, Request, Response } from '@frontastic/extension-types';
-import { AccountApi } from '../apis/AccountApi';
-import { getCurrency, getLocale } from '../utils/Request';
-import { CartFetcher } from '../utils/CartFetcher';
-import { EmailApiFactory } from '../utils/EmailApiFactory';
+import { Request } from '@frontastic/extension-types';
 import { Address } from '@Types/account/Address';
 import { Account } from '@Types/account/Account';
-import { ExternalError } from '@Commerce-commercetools/utils/Errors';
 
 export * from './BaseAccountController';
-
-type ActionHook = (request: Request, actionContext: ActionContext) => Promise<Response>;
 
 export type AccountRegisterBody = {
   email?: string;
@@ -32,18 +24,6 @@ type AccountLoginBody = {
   password?: string;
   businessUnitKey?: string;
 };
-
-async function loginAccount(request: Request, actionContext: ActionContext, account: Account, businessUnitKey = '') {
-  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request), getCurrency(request));
-
-  const cart = await CartFetcher.fetchCart(request, actionContext);
-
-  try {
-    return await accountApi.login(account, cart);
-  } catch (e) {
-    throw e;
-  }
-}
 
 function parseBirthday(accountRegisterBody: AccountRegisterBody): Date | undefined {
   if (accountRegisterBody.birthdayYear) {
@@ -88,116 +68,3 @@ function mapRequestToAccount(request: Request): Account {
 
   return account;
 }
-
-export const register: ActionHook = async (request: Request, actionContext: ActionContext) => {
-  const locale = getLocale(request);
-
-  const accountApi = new AccountApi(actionContext.frontasticContext, locale, getCurrency(request));
-
-  const accountData = mapRequestToAccount(request);
-
-  const cart = await CartFetcher.fetchCart(request, actionContext).catch(() => undefined);
-
-  try {
-    const account = await accountApi.create(accountData, cart);
-
-    const emailApi = EmailApiFactory.getDefaultApi(actionContext.frontasticContext, locale);
-
-    emailApi.sendWelcomeCustomerEmail(account);
-
-    emailApi.sendAccountVerificationEmail(account);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ accountId: account.accountId }),
-      sessionData: {
-        ...request.sessionData,
-      },
-    };
-  } catch (error) {
-    if (error instanceof AccountAuthenticationError || error instanceof ExternalError) {
-      return {
-        statusCode: 400,
-        sessionData: {
-          ...request.sessionData,
-        },
-        error: error.message,
-      };
-    }
-
-    return {
-      statusCode: 400,
-      sessionData: {
-        ...request.sessionData,
-      },
-    };
-  }
-};
-
-export const login: ActionHook = async (request, actionContext) => {
-  const accountLoginBody: AccountLoginBody = JSON.parse(request.body);
-
-  const loginInfo: Account = {
-    email: accountLoginBody.email,
-    password: accountLoginBody.password,
-  };
-
-  try {
-    const account = await loginAccount(request, actionContext, loginInfo, accountLoginBody.businessUnitKey);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(account),
-      sessionData: {
-        ...request.sessionData,
-        account,
-      },
-    };
-  } catch (error) {
-    if (error instanceof AccountAuthenticationError || error instanceof ExternalError) {
-      return {
-        statusCode: 400,
-        sessionData: {
-          ...request.sessionData,
-        },
-        error: error.message,
-      };
-    }
-
-    return {
-      statusCode: 400,
-      sessionData: {
-        ...request.sessionData,
-      },
-    };
-  }
-};
-
-/**
- * Reset password
- */
-export const reset: ActionHook = async (request, actionContext) => {
-  type AccountResetBody = {
-    token?: string;
-    newPassword?: string;
-  };
-
-  const accountResetBody: AccountResetBody = JSON.parse(request.body);
-
-  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request), getCurrency(request));
-
-  const newAccount = await accountApi.resetPassword(accountResetBody.token, accountResetBody.newPassword);
-  newAccount.password = accountResetBody.newPassword;
-
-  // TODO: do we need to log in the account after creation?
-  // TODO: handle exception when customer can't login if email is not confirmed
-  const account = await loginAccount(request, actionContext, newAccount);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(account),
-    sessionData: {
-      ...request.sessionData,
-      account,
-    },
-  };
-};
