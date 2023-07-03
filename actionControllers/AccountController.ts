61394fa6314da407b4b92aa11cae334de1a38ade
JoelFickson <Joelngozo@gmail.com>
2023-07-03 16:19:17 +0200

    feat: update AccountController

--- actionControllers/AccountController.ts
+++ actionControllers/AccountController.ts
@@ -1,6 +1,4 @@
-import { Request } from '@frontastic/extension-types';
 import { Address } from '@Types/account/Address';
-import { Account } from '@Types/account/Account';
 
 export * from './BaseAccountController';
 
@@ -19,52 +17,11 @@ export type AccountRegisterBody = {
   shippingAddress?: Address;
 };
 
-type AccountLoginBody = {
+export type AccountLoginBody = {
   email?: string;
   password?: string;
   businessUnitKey?: string;
 };
 
-function parseBirthday(accountRegisterBody: AccountRegisterBody): Date | undefined {
-  if (accountRegisterBody.birthdayYear) {
-    return new Date(
-      +accountRegisterBody.birthdayYear,
-      +accountRegisterBody?.birthdayMonth ?? 1,
-      +accountRegisterBody?.birthdayDay ?? 1,
-    );
-  }
 
-  return null;
-}
 
-function mapRequestToAccount(request: Request): Account {
-  const accountRegisterBody: AccountRegisterBody = JSON.parse(request.body);
-
-  const account: Account = {
-    email: accountRegisterBody?.email,
-    confirmed: accountRegisterBody?.confirmed,
-    password: accountRegisterBody?.password,
-    salutation: accountRegisterBody?.salutation,
-    firstName: accountRegisterBody?.firstName,
-    lastName: accountRegisterBody?.lastName,
-    company: accountRegisterBody?.company,
-    birthday: parseBirthday(accountRegisterBody),
-    addresses: [],
-  };
-
-  if (accountRegisterBody.billingAddress) {
-    accountRegisterBody.billingAddress.isDefaultBillingAddress = true;
-    accountRegisterBody.billingAddress.isDefaultShippingAddress = !(accountRegisterBody.shippingAddress !== undefined);
-
-    account.addresses.push(accountRegisterBody.billingAddress);
-  }
-
-  if (accountRegisterBody.shippingAddress) {
-    accountRegisterBody.shippingAddress.isDefaultShippingAddress = true;
-    accountRegisterBody.shippingAddress.isDefaultBillingAddress = !(accountRegisterBody.billingAddress !== undefined);
-
-    account.addresses.push(accountRegisterBody.shippingAddress);
-  }
-
-  return account;
-}
