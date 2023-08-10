d8f5e89e0f7e8750f9295c27b187797309f7a162
Carlos Mourullo <carlosmourullo@gmail.com>
2023-08-10 18:18:41 +0200

    chore: removed unused class

--- actionControllers/BaseWishlistController.ts
+++ actionControllers/BaseWishlistController.ts
@@ -1,4 +1,5 @@
 import { ActionContext, Request, Response } from '@frontastic/extension-types';
+import { Guid } from '../utils/Guid';
 import { Account } from '@Types/account/Account';
 import { getCurrency, getLocale } from '../utils/Request';
 import { BaseWishlistApi as WishlistApi } from '../apis/BaseWishlistApi';
