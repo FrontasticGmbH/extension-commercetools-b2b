e68ad945cfdca4e84b362284c8776fd0aa36e7b2
Carlos Mourullo <carlosmourullo@gmail.com>
2025-06-09 17:03:16 +0200

    Released version 2.5.0
    *• New Features & Improvements **
    - Get storeKey using the method to look into the query and the session
    - Switch method to standard class method for uniformity
    - View all wishlists for associates in same business units
    - Show full acount information on wishlist
    - Migrate from commercetools/sdk-client-v2 to commercetools/ts-client
    - Added approvalStatus for orders with approval flow
    - Validated accountId before we build cart api
    *• Bug Fixes **
    - Get the error code from statusCode instead of code

--- actionControllers/WishlistController.ts
+++ actionControllers/WishlistController.ts
@@ -7,7 +7,7 @@ import queryParamsToIds from '@Commerce-commercetools/utils/requestHandlers/quer
 import { ValidationError } from '@Commerce-commercetools/errors/ValidationError';
 import getWishlistApi from '@Commerce-commercetools/utils/apiConstructors/getWishlistApi';
 import WishlistApi from '@Commerce-commercetools/apis/WishlistApi';
-import { getStoreKey } from '@Commerce-commercetools/utils/requestHandlers/Request';
+import { getBusinessUnitKey, getStoreKey } from '@Commerce-commercetools/utils/requestHandlers/Request';
 import { AccountFetcher } from '@Commerce-commercetools/utils/AccountFetcher';
 type ActionHook = (request: Request, actionContext: ActionContext) => Promise<Response>;
 
@@ -53,13 +53,16 @@ export const queryWishlists: ActionHook = async (request, actionContext) => {
     const wishlistApi = getWishlistApi(request, actionContext.frontasticContext);
 
     const accountId = AccountFetcher.fetchAccountIdFromSessionEnsureLoggedIn(request);
+    const storeKey = getStoreKey(request);
+    const businessUnitKey = getBusinessUnitKey(request);
 
     const wishlistQuery: WishlistQuery = {
       name: request.query?.name ?? undefined,
       accountId,
       limit: request.query?.limit ?? undefined,
       cursor: request.query?.cursor ?? undefined,
-      storeKey: request.query?.storeKey ?? undefined,
+      storeKey,
+      businessUnitKey,
       wishlistIds: queryParamsToIds('wishlistIds', request.query),
       query: request.query?.query ?? undefined,
     };
--- mappers/WishlistMapper.ts
+++ mappers/WishlistMapper.ts
@@ -2,6 +2,7 @@ import { Wishlist } from '@Types/wishlist/Wishlist';
 import { ShoppingList, ShoppingListLineItem, StoreKeyReference } from '@commercetools/platform-sdk';
 import { Store } from '@Types/store/Store';
 import { LineItem } from '@Types/wishlist';
+import AccountMapper from './AccountMapper';
 import { Locale } from '@Commerce-commercetools/interfaces/Locale';
 import ProductMapper from '@Commerce-commercetools/mappers/ProductMapper';
 import ProductRouter from '@Commerce-commercetools/utils/routers/ProductRouter';
@@ -18,15 +19,16 @@ export class WishlistMapper {
       wishlistId: commercetoolsShoppingList.id,
       wishlistVersion: commercetoolsShoppingList.version.toString(),
       key: commercetoolsShoppingList?.key,
-      accountId: commercetoolsShoppingList.customer?.id ?? undefined,
-      name: commercetoolsShoppingList.name[locale.language],
-      description: commercetoolsShoppingList.description?.[locale.language],
+      account: AccountMapper.commercetoolsCustomerToAccount(commercetoolsShoppingList.customer?.obj) ?? undefined,
+      name: LocalizedValue.getLocalizedValue(locale, defaultLocale, commercetoolsShoppingList.name),
+      description: LocalizedValue.getLocalizedValue(locale, defaultLocale, commercetoolsShoppingList.description),
       lineItems: (commercetoolsShoppingList.lineItems || []).map((lineItem) =>
         this.commercetoolsLineItemToLineItem(lineItem, locale, supplyChannelId),
       ),
       slug: LocalizedValue.getLocalizedValue(locale, defaultLocale, commercetoolsShoppingList.slug),
       store: this.commercetoolsStoreRefToStore(commercetoolsShoppingList.store),
       businessUnitKey: commercetoolsShoppingList.businessUnit?.key,
+      createdAt: commercetoolsShoppingList.createdAt,
     };
   };
 
